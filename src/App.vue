867626171967b9c86826313e05ce541fd520478f
Wayne <winson.zhang@aol.com>
2025-07-08 18:00:34 +0800

    feat:继续学习

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
 <template>
   <!-- 务必看懂下面这一行代码 -->
   <!-- <h2 a="1+1" :b="1 + 1" c="x" :d="x" ref="qwe">测试</h2> -->
-  <Person a="哈哈" />
+  <button @click="show">切换显示</button>
+  <Person a="哈哈" v-if="isShow" />
 </template>
 
 <script lang="ts" setup name="App">
 import Person from '@/components/Person.vue'
-import { reactive } from 'vue'
-import type { Persons } from '@/types'
+import { ref } from 'vue'
 
-let x = 9
+let isShow = ref(true)
 
-let personList = reactive<Persons>([
-  {
-    id: '001',
-    name: '张三',
-    age: 25,
-  },
-  {
-    id: '002',
-    name: '李四',
-    age: 30,
-  },
-  {
-    id: '003',
-    name: '王五',
-    age: 28,
-  },
-])
-
-console.log(personList)
+function show() {
+  isShow.value = !isShow.value
+}
 </script>
 
 <style></style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,26 +1,52 @@
 <template>
   <div class="person">
-    <ul>
-      <li v-for="item in list" :key="item.id">{{ item.name }} - {{ item.age }}</li>
-    </ul>
+    <h2>当前求和为: {{ sum }}</h2>
+    <button @click="add">点我 sum+1</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import type { Persons } from '@/types'
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue'
+let sum = ref(0)
 
-// 接收a
-// defineProps(['a'])
+function add() {
+  sum.value += 1
+}
 
-// 接收a和 list，同时将 props 保存起来
-// const props = defineProps(['a', 'list'])
+// 创建
+console.log('Person组件被创建了')
+// 挂载前
+onBeforeMount(() => {
+  console.log('Person组件挂载前')
+})
+// 挂载后
+onMounted(() => {
+  console.log('Person组件挂载后')
+})
+// 更新前
+onBeforeUpdate(() => {
+  console.log('Person组件更新前')
+})
+// 更新后
+onUpdated(() => {
+  console.log('Person组件更新后')
+})
 
-// 接收 list +限制类型
-// const props = defineProps<{ list: Persons }>()
-
-// 接收限制类型 + 限制类型 + 限制必要性 + 指定默认值
-const props = withDefaults(defineProps<{ list?: Persons }>(), {
-  list: () => [{ id: '001', name: '康师傅·王麻子·特仑苏', age: 40 }],
+// 卸载前
+onBeforeUnmount(() => {
+  console.log('Person组件卸载前')
+})
+// 卸载后
+onUnmounted(() => {
+  console.log('Person组件卸载后')
 })
 </script>
 
